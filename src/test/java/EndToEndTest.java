061c0ef8995f0cd33757ddfefe33ca2363ae9af0
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2025-01-08 18:18:20 +0300

    Login Test Added and edited gitignore

--- src/test/java/EndToEndTest.java
+++ src/test/java/EndToEndTest.java
@@ -1,3 +1,18 @@
+import org.testng.annotations.Test;
+
 public class EndToEndTest extends BaseTest{
 
+    HomePage homePage;
+
+
+
+
+
+
+    @Test(priority = 1)
+    public void logInTest(){
+        homePage = new HomePage(driver);
+
+        homePage.goToLogin();
+    }
 }
