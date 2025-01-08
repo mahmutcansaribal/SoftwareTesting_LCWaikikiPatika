5e454fbf15c663092a2750134863be038fe811ac
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2025-01-08 18:47:39 +0300

    Created CategorySelection

---
+++ src/main/java/CategorySelection.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CategorySelection extends BasePage{
+    @FindBy(xpath = "//a[normalize-space()='ÇOCUK & BEBEK']")
+    WebElement headerItem;
+
+    @FindBy(xpath = "//span[normalize-space()='KIZ ÇOCUK']")
+    WebElement tabHeaderItem;
+    By selectCategoryZoneItem = By.xpath("//section[contains(@class,'content-tab')]//a[contains(@class,'')][normalize-space()='Mont ve Kaban']");
+
+    public CategorySelection(WebDriver driver){
+        super(driver);
+        this.actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void moveToHeaderItem() {
+        if (actions != null) {
+            wait.until(ExpectedConditions.visibilityOf(headerItem));  // Öğenin görünür olmasını bekleyin
+            if (headerItem.isDisplayed()) {  // Eğer öğe görünüyorsa
+                actions.moveToElement(headerItem).perform(); // Hover işlemi
+            } else {
+                System.out.println("Öğe görünür değil!");
+            }
+        } else {
+            System.out.println("Actions nesnesi null!");
+        }
+    }
+    public void hoverOverTabHeaderItem(){
+        wait.until(ExpectedConditions.visibilityOf(tabHeaderItem));
+        actions.moveToElement(tabHeaderItem).perform();
+    }
+
+    public void clickOnCategoryZoneItem(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(selectCategoryZoneItem));
+        click(selectCategoryZoneItem);
+    }
+
+}
