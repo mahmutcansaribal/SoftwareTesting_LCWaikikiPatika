6aac02de2c4b06cb0528c02dab97f9bbbe95df62
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2025-01-13 16:59:11 +0300

    Added LogOnTest

---
+++ src/test/java/tests/LogOnTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+import io.qameta.allure.Step;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogOnPage;
+
+public class LogOnTest extends BaseTest {
+    LogOnPage logOnPage;
+    HomePage homePage;
+
+    @Test
+    @Description("Test Description: Logging in with valid credentials.")
+    @Severity(SeverityLevel.CRITICAL)
+    public void logOnTest(){
+        logOnPage = new LogOnPage(driver);
+        homePage = new HomePage(driver);
+
+        goToLoginPage();
+        performLogin();
+    }
+
+    @Step("Go to login Page")
+    public void goToLoginPage(){
+        homePage.goToLoginPage();
+    }
+    @Step("Enter email and click on 'Continue' button")
+    public void setEmailAndContinue() {
+        logOnPage.setEmail();
+        logOnPage.setContinueBtn();
+    }
+    @Step("Enter password and click on 'Login' button")
+    public void performLogin() {
+        setEmailAndContinue();
+        logOnPage.setPassword();
+        logOnPage.setLoginBtn();
+    }
+}
