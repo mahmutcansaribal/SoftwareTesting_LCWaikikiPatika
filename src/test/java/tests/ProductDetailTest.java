a0ae3b538e0dcca5f1627d7a2ef64698b65d76e5
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2025-01-13 17:01:26 +0300

    Added ProductDetailTest

---
+++ src/test/java/tests/ProductDetailTest.java
@@ -0,0 +1,80 @@
+package tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import org.testng.annotations.Test;
+import pages.*;
+
+public class ProductDetailTest extends BaseTest{
+
+    HomePage homePage;
+    LogOnPage logOnPage;
+    CategorySelection categorySelection;
+    ProductPage productPage;
+    ProductDetailsPage productDetailsPage;
+
+
+    @Test
+    @Description("Test Description: Select a body size for the product and add it to the cart.")
+    @Severity(SeverityLevel.CRITICAL)
+    public void AddProductToCart(){
+        homePage = new HomePage(driver);
+        logOnPage = new LogOnPage(driver);
+        categorySelection = new CategorySelection(driver);
+        productPage = new ProductPage(driver);
+        productDetailsPage = new ProductDetailsPage(driver);
+
+        goToLoginPage();
+        performLogin();
+        selectCategory();
+        applyFiltersAndSorting();
+        selectProduct(3);
+        selectBodySize(1);
+        addToCart();
+
+    }
+    @Step("Go to the login page")
+    public void goToLoginPage() {
+        homePage.goToLoginPage();
+    }
+
+    @Step("Log in with username and password")
+    public void performLogin() {
+        logOnPage.setEmail();
+        logOnPage.setContinueBtn();
+        logOnPage.setPassword();
+        logOnPage.setLoginBtn();
+    }
+
+    @Step("Select a category")
+    public void selectCategory() {
+        categorySelection.moveToHeaderItem();
+        categorySelection.hoverOverTabHeaderItem();
+        categorySelection.clickOnCategoryZoneItem();
+    }
+    @Step("Apply filters and sorting options")
+    public void applyFiltersAndSorting() {
+        productPage.setFilterOptionBody5_6();
+        productPage.setFilterOptionBody6_7();
+        productPage.setFilterOptionBody6();
+        productPage.setSelectColor();
+        productPage.setSelectSortBtn();
+        productPage.setBestSellers();
+    }
+    @Step("Select product number {0} from the list")
+    public void selectProduct(int productIndex) {
+        productPage.selectProduct(productIndex);
+    }
+
+    @Step("Select body size option {0}")
+    public void selectBodySize(int bodySizeIndex) {
+        productDetailsPage.selectBodySize(bodySizeIndex);
+    }
+
+    @Step("Add the selected product to the cart")
+    public void addToCart() {
+        productDetailsPage.addToCart();
+    }
+}
